71d5ce3c555a6a192064bae08d1ff2c98a66350a
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-07-08 23:59:38 +0200

    mongoose setup

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { mongoURI } = require('./config/keys');
 
 const app = express();
+
+mongoose
+	.connect(mongoURI)
+	.then(() => {
+		console.log('DB connected');
+	})
+	.catch(err => {
+		console.log(err);
+	});
 
 app.get('/', (req, res) => {
 	res.send({ hello: 'hello' });
